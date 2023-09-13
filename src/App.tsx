7dda1d1437051195bf9d62d38c6ade66e5de5179
Largopie <106071687+Largopie@users.noreply.github.com>
2023-09-13 16:18:05 +0900

    refactor: 디자인 변경으로 인한 CSS Layout 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,44 @@
-import HeaderSearch from 'pages/mainPage/HeaderSearch';
-import HeaderLogin from 'pages/mainPage/HeaderLogin';
 import React from 'react';
 import styled from 'styled-components';
-import AdBar from 'pages/mainPage/AdBar';
-import MainContent from 'pages/mainPage/MainContent';
 
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
+const MainContainer = styled.div`
+  padding: 3% 0px 3% 0px;
   width: 100vw;
   height: 100vh;
+  background-color: #E8EAEE;
 `
-
-const TopStyle = styled.div`
-  position: absolute;
-  top: 0px;
-  background-color: #DADADA;
-  width: 100vw;
-  height: 36px;
-`
-const HeaderStyle = styled.div`
-  position: absolute;
-  top: 36px;
-  background-color: #FFFFFF;
-  width: 100vw;
-  height: 64px;
-`
-
-const AdBarStyle = styled.div`
-  position: absolute;
-  top: 100px;
-  background-color: #2878FF;
-  width: 100vw;
-  height: 160px;
-`;
-
-const Content = styled.div`
+const SubContainer = styled.div`
+  width: 100%;
+  height: 100%;
   display: flex;
-  flex-direction: column;
-  position: relative;
-  z-index: 1;
-  width: 1280px;
-  height: 400px;
+  justify-content: space-evenly;
+  
+`
+
+const LeftContainer = styled.div`
+  width: 12%;
+  background-color: aqua;
+`
+
+const CenterCotainer = styled.div`
+  width: 55%;
+  background-color: green;
+`
+
+const RightContainer = styled.div`
+  width: 20%;
+  background-color: yellow;
 `
 
 function App() {
   return (
-    <Container>
-      <TopStyle />
-      <HeaderStyle />
-      <AdBarStyle />
-      <Content>
-        <HeaderLogin />
-        <HeaderSearch />
-        <AdBar />
-        <MainContent />
-      </Content>
-    </Container>
+    <MainContainer>
+      <SubContainer>
+        <LeftContainer />
+        <CenterCotainer />
+        <RightContainer />
+      </SubContainer>
+    </MainContainer>
   );
 }
 
